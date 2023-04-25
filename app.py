c9993372add784f5a547632cb7d6dc5263ee45aa
Alfred <jajimenez@baufest.com>
2023-04-25 00:16:39 -0600

    intento 20

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-
-from flask import Flask, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def hello_world():
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=5000)
